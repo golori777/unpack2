e39714be867a94ae5f0e6493dc39090a698f9a3c
golori777 <shige627@gmail.com>
2021-12-15 16:15:11 +0900

    no message

---
+++ app/src/main/java/com/example/unpack/database/MemoDao.kt
@@ -0,0 +1,25 @@
+package com.example.unpack.database
+
+import androidx.room.*
+
+// DAOには @Dao をつける
+@Dao
+interface MemoDao {
+    // @Queryを使うと実行SQLを定義することができる
+    @Query("SELECT * from memo")
+    fun getAllMemos(): List<Memo>
+
+    // SQLに引数を渡す場合は 「:<引数名>」 で関連付ける
+    @Query("SELECT * from memo where id = :id LIMIT 1")
+    fun findById(id: String): Memo
+
+    // @Insertなど用意されたアノテーションをつけると引数にEntityを渡すだけで処理ができる
+    @Insert
+    fun insert(memo: Memo)
+
+    @Update
+    fun update(memo: Memo)
+
+    @Delete
+    fun delete(memo: Memo)
+}
