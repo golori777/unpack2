6dd85ec61130f69db3c6105d1c4a917a9dff7413
horii <s-horii@forgevision.com>
2021-12-24 10:46:02 +0900

    no message

--- app/src/main/java/com/example/unpack/database/MemoDao.kt
+++ app/src/main/java/com/example/unpack/database/MemoDao.kt
@@ -13,6 +13,10 @@ interface MemoDao {
     @Query("SELECT * from memo where id = :id LIMIT 1")
     fun findById(id: String): Memo
 
+    // SQLに引数を渡す場合は 「:<引数名>」 で関連付ける
+    @Query("DELETE from memo where id = :id")
+    fun deleteById(id: Int): Int
+
     // @Insertなど用意されたアノテーションをつけると引数にEntityを渡すだけで処理ができる
     @Insert
     fun insert(memo: Memo)
